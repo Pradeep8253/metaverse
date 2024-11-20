8bb2fc19e42a2a5a2223311a446ad92ec55be1e1
Pradeep8253 <89512667+Pradeep8253@users.noreply.github.com>
2024-11-20 22:57:56 +0530

    api endpoints done

--- apps/http/src/routes/v1/admin.ts
+++ apps/http/src/routes/v1/admin.ts
@@ -5,3 +5,16 @@ export const adminRouter = Router();
 adminRouter.post("/element" , (req , res) => {
 
 } )
+
+adminRouter.put("/element/:elementId" , (req ,res) =>{
+
+})
+
+adminRouter.get("/avatar" , (req ,res) => {
+
+})
+
+
+adminRouter.get("/map" , (req ,res) => {
+
+})
--- apps/http/src/routes/v1/space.ts
+++ apps/http/src/routes/v1/space.ts
@@ -1,3 +1,27 @@
 import { Router } from "express";
 
 export const spaceRouter = Router() ;
+
+spaceRouter.get("/all" , (req , res) => {
+
+})
+spaceRouter.get("/:spaceId" , (req ,res) => {
+    
+})
+
+spaceRouter.post("/" , (req , res) => {
+
+})
+
+spaceRouter.post("/element" , (req , res) => {
+
+})
+
+spaceRouter.delete("/space/:spaceId" , (req ,res) => {
+
+})
+
+spaceRouter.delete("/element" , (req ,res) => {
+
+})
+
